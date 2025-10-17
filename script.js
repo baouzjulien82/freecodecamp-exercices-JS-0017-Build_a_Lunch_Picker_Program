a9b02c2328c71372576508dcbf30654618dcfdcc
Julien Baouz <baouzjulien@gmail.com>
2025-10-17 20:35:05 +0200

    Implement lunch menu management functions

---
+++ script.js
@@ -0,0 +1,50 @@
+const lunches = [];
+// ajouter un lunch à la fin du tableau
+const addLunchToEnd = (array, string) => {
+  array.push(string);
+  console.log(`${string} added to the end of the lunch menu.`);
+  return array;
+};
+// ajouter un lunch au début du tableau
+const addLunchToStart = (array, string) => {
+  array.unshift(string);
+  console.log(`${string} added to the start of the lunch menu.`);
+  return array;
+};
+// supprimer un lunch de la fin du tableau
+const removeLastLunch = (array) => {
+  if(array.length !== 0) {
+  let deletedItem = array.pop();
+  console.log(`${deletedItem} removed from the end of the lunch menu.`);
+  } else {
+    console.log("No lunches to remove.");
+  }
+  return array;
+};
+// supprimer un lunch du début du tableau
+const removeFirstLunch = (array) => {
+  if(array.length !== 0) {
+  let deletedItem = array.shift();
+  console.log(`${deletedItem} removed from the start of the lunch menu.`);
+  } else {
+    console.log("No lunches to remove.");
+  }
+  return array;
+};
+// random select a lunch
+const getRandomLunch = (array) => {
+  if(array.length !== 0) {
+    console.log(`Randomly selected lunch: ${array[math.floor(math.random() * array.length)]}`);
+  } else {
+    console.log("No lunches available.");
+  }
+};
+
+// show lunchMenu
+const showLunchMenu = (array) => {
+  if(array.length !== 0) {
+    console.log(`Menu items: ${array}`);
+  } else {
+    console.log("The menu is empty.");
+  }
+};
